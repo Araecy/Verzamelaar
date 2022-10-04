f5445d711618dad753fe797d95de8bf13b82e718
87243 <87243@glr.nl>
2022-10-04 14:51:08 +0200

    update

---
+++ js/signupAndLoginScript.js
@@ -0,0 +1,21 @@
+// Signup:
+// Get the modal
+let modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+};
+
+// Login:
+// Get the modal
+let modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
